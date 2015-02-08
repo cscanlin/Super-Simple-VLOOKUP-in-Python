33bfa487d56fb7e5ef1fc3916aa52c025a6a3579
cscanlin <cscanlin@gmail.com>
2015-02-07 19:28:14 -0800

    Update vlookup_example.py

--- vlookup_example.py
+++ vlookup_example.py
@@ -1,8 +1,8 @@
-import vlookup
+import python_vlookup
 
 lookup_value = 'outdoor/accessories'
 lookup_list = ['bed-and-bath/accessories','bed-and-bath/storage-and-hampers/storage','decor-and-pillows/rugs/hide']
 
 
-print vlookup.vlookup(lookup_value,'example.csv',2)
-print vlookup.vlookup(lookup_list,'example.csv',3)
+print python_vlookup.vlookup(lookup_value,'example.csv',2)
+print python_vlookup.vlookup(lookup_list,'example.csv',3)
