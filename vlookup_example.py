4fa7af3598629d0572d011a1f08ce6bab37d040a
cscanlin <cscanlin@gmail.com>
2015-02-07 17:10:58 -0800

    Create vlookup_example.py
    Example Usage - uses example.csv as lookup

---
+++ vlookup_example.py
@@ -0,0 +1,8 @@
+import vlookup
+
+lookup_value = 'outdoor/accessories'
+lookup_list = ['bed-and-bath/accessories','bed-and-bath/storage-and-hampers/storage','decor-and-pillows/rugs/hide']
+
+
+print vlookup.vlookup(lookup_value,'example.csv',2)
+print vlookup.list_vlookup(lookup_list,'example.csv',3)
