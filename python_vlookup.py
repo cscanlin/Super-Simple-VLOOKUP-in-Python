c7db5a13f0248e2f7787e3d062215578537d698c
cscanlin <cscanlin@gmail.com>
2015-02-07 17:43:38 -0800

    Update python_vlookup.py
    Combined vlookup and vlookup_list

--- python_vlookup.py
+++ python_vlookup.py
@@ -14,19 +14,17 @@ def create_lookup_dict(lookup_array,lookup_index):
         lookup_dict[lookup_entry[0]] = lookup_entry[int(lookup_index)]
     return lookup_dict
 
-
 def vlookup(lookup_value,lookup_csv,col_index_num):
     lookup_array = create_lookup_array(lookup_csv)
     lookup_dict = create_lookup_dict(lookup_array,col_index_num-1)
 
-    return lookup_dict[lookup_value]
+    if isinstance(lookup_value, list):
 
-def list_vlookup(lookup_value_list,lookup_csv,col_index_num):
-    lookup_array = create_lookup_array(lookup_csv)
-    lookup_dict = create_lookup_dict(lookup_array,col_index_num-1)
+        lookup_value_list = lookup_value
+        looked_up_list = []
 
-    looked_up_list = []
-
-    for lookup_value in lookup_value_list:
-        looked_up_list.append(lookup_dict[lookup_value])
-    return looked_up_list
+        for lookup_value in lookup_value_list:
+            looked_up_list.append(lookup_dict[lookup_value])
+        return looked_up_list
+    else:
+        return lookup_dict[lookup_value]
