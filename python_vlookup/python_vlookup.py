1a8dbdc5a280d72abf61b8d7eff8c5b1edad47ed
Christopher Scanlin <cscanlin@gmail.com>
2015-06-02 13:01:16 -0700

    stable version with debug

--- python_vlookup/python_vlookup.py
+++ python_vlookup/python_vlookup.py
@@ -1,9 +1,10 @@
 import csv
+from collections import Iterable
 
-# Returns a list of lists, where each row is a sublist
+# Returns a list of lists, where each row of the csv is a sublist
 def get_csv_data(csv_file_path):
-    with open(csv_file_path, 'rU') as csvfile:
-        csv_rows = list(csv.reader(csvfile, delimiter=',', quotechar='|'))
+    with open(csv_file_path, 'rb') as csvfile:
+        csv_rows = list(csv.reader(csvfile))
     return csv_rows
 
 # Creates a dictionary with the first list item as the key,
@@ -12,21 +13,55 @@ def create_column_dict(csv_rows, index):
     column_dict = {row[0]: row[index] for row in csv_rows}
     return column_dict
 
-# Returns the Looked up value as a list or string
-def vlookup(item,csv_file_path,col_index_num):
-    csv_rows = get_csv_data(csv_file_path)
-    column_dict = create_column_dict(csv_rows,col_index_num-1)
-
-    if isinstance(item, list):
-        return [column_dict[str(key)] for key in item]
-
-    return column_dict[str(item)]
-
 # faster_vlookup allows you to call each function individually.
 # You can use the two helper functions to create your array and dictionary only once,
 # and then call faster_vlookup which is a much quicker
 # Useful for using in loops.
 def faster_vlookup(item,column_dict):
-    if isinstance(item, list):
-        return [column_dict[str(key)] for key in item]
-    return column_dict[str(item)]
+    try:
+        if isinstance(item, Iterable) and not isinstance(item, str):
+            return [column_dict[str(entry)] for entry in item]
+        return column_dict[str(item)]
+    except KeyError, item:
+        raise
+
+# Returns the Looked up value as a list or string, uses excel column index numbering (no 0 column).
+# If debug is on the user is prompted to enter values for missing entries
+def vlookup(item,csv_file_path,col_index_num,debug=None):
+    csv_rows = get_csv_data(csv_file_path)
+    column_dict = create_column_dict(csv_rows,col_index_num-1)
+
+    try:
+        if isinstance(item, Iterable) and not isinstance(item, str):
+            return [column_dict[str(entry)] for entry in item]
+        return column_dict[str(item)]
+    except KeyError, e:
+        if debug=='fix':
+            entry = str(e)[1:-1]
+            debug_lookup(entry,csv_file_path)
+            return vlookup(item,csv_file_path,col_index_num,debug='fix')
+        if debug=='skip':
+            return_list = []
+            if isinstance(item, Iterable) and not isinstance(item, str):
+                for entry in item:
+                    try:
+                        return_list.append(column_dict[str(entry)])
+                    except:
+                        return_list.append('#N/A')
+                return return_list
+            return '#N/A'
+        else:
+             raise
+
+def debug_lookup(entry,csv_file_path):
+    print 'LOOKUP ENTRY MISSING'
+    new_entry = [entry]
+    with open(csv_file_path,'rb') as csvfile:
+        reader = csv.reader(csvfile)
+        for column_heading in next(reader)[1:]:
+            print 'Enter {0} for {1}:'.format(column_heading,entry)
+            column_entry = raw_input()
+            new_entry.append(column_entry)
+    with open(csv_file_path,'ab') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(new_entry)
--- vlookup_example.py
+++ vlookup_example.py
@@ -1,15 +1,24 @@
-from python_vlookup import python_vlookup
+import python_vlookup
 
-###for faster_vlookup
+value_to_lookup = 'bed-and-bath/accessories'
+value_list = ['bed-and-bath/accessories','bed-and-bath/storage-and-hampers/storage','new_entry_x','new_entry_y',1]
+
+def generate_values(value_list):
+    for value in value_list:
+        yield value
+
+### Basic VLOOKUP, just save your lookup tables as a csv and do it
+print python_vlookup.vlookup(value_to_lookup,'example.csv',2)
+
+### Runs Through the debug
+print python_vlookup.vlookup(value_list,'example.csv',3,debug='skip')
+
+print python_vlookup.vlookup(value_list,'example.csv',3,debug='fix')
+
+
+### Faster_vlookup, can take in almost any iterable and returns an iterable
 csv_rows = python_vlookup.get_csv_data('example.csv')
 column_dict = python_vlookup.create_column_dict(csv_rows,1)
-###
 
-lookup_value = 'outdoor/accessories'
-lookup_list = ['bed-and-bath/accessories','bed-and-bath/storage-and-hampers/storage','decor-and-pillows/rugs/hide']
-
-
-print python_vlookup.vlookup(lookup_value,'example.csv',2)
-print python_vlookup.vlookup(lookup_list,'example.csv',3)
-
-print python_vlookup.faster_vlookup(lookup_list,column_dict)
+for matched_value in python_vlookup.faster_vlookup(generate_values(value_list),column_dict):
+    print matched_value
