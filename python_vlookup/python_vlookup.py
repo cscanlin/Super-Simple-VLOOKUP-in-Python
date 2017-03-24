1aa8d11d7b9cbec6fceb6d6ee8d9880cacdaeea0
Chris Scanlin <cscanlin@gmail.com>
2017-03-24 13:47:03 -0700

    various pep8 fixes, and update for python3

--- python_vlookup/python_vlookup.py
+++ python_vlookup/python_vlookup.py
@@ -1,10 +1,13 @@
-import csv
+from __future__ import print_function
+from builtins import input
 from collections import Iterable
+
+import csv
 
 # Returns a list of lists, where each row of the csv is a sublist
 def get_csv_data(csv_file_path):
-    with open(csv_file_path, 'rb') as csvfile:
-        csv_rows = list(csv.reader(csvfile))
+    with open(csv_file_path, 'r') as csvfile:
+        csv_rows = [row for row in csv.reader(csvfile.read().splitlines())]
     return csv_rows
 
 # Creates a dictionary with the first list item as the key,
@@ -17,51 +20,54 @@ def create_column_dict(csv_rows, index):
 # You can use the two helper functions to create your array and dictionary only once,
 # and then call faster_vlookup which is a much quicker
 # Useful for using in loops.
-def faster_vlookup(item,column_dict):
+def faster_vlookup(item, column_dict, debug=None, error_value=None):
     try:
         if isinstance(item, Iterable) and not isinstance(item, str):
             return [column_dict[str(entry)] for entry in item]
-        return column_dict[str(item)]
-    except KeyError, item:
-        raise
+        else:
+            return column_dict[str(item)]
+    except KeyError:
+        if debug == 'skip':
+            if isinstance(item, Iterable) and not isinstance(item, str):
+                    return [column_dict[str(entry)] if str(entry) in column_dict else error_value for entry in item]
+            else:
+                return error_value
+        else:
+            raise
 
 # Returns the Looked up value as a list or string, uses excel column index numbering (no 0 column).
-# If debug is on the user is prompted to enter values for missing entries
-def vlookup(item,csv_file_path,col_index_num,debug=None):
+# If debug is on, the user is prompted to enter values for missing entries
+def vlookup(item, csv_file_path, col_index_num, debug=None, error_value=None):
     csv_rows = get_csv_data(csv_file_path)
-    column_dict = create_column_dict(csv_rows,col_index_num-1)
+    column_dict = create_column_dict(csv_rows, col_index_num-1)
 
     try:
         if isinstance(item, Iterable) and not isinstance(item, str):
             return [column_dict[str(entry)] for entry in item]
-        return column_dict[str(item)]
-    except KeyError, e:
-        if debug=='fix':
-            entry = str(e)[1:-1]
-            debug_lookup(entry,csv_file_path)
-            return vlookup(item,csv_file_path,col_index_num,debug='fix')
-        if debug=='skip':
-            return_list = []
-            if isinstance(item, Iterable) and not isinstance(item, str):
-                for entry in item:
-                    try:
-                        return_list.append(column_dict[str(entry)])
-                    except:
-                        return_list.append('#N/A')
-                return return_list
-            return '#N/A'
         else:
-             raise
+            return column_dict[str(item)]
+    except KeyError as e:
+        if debug == 'fix':
+            entry = str(e)[1:-1]
+            debug_lookup(entry, csv_file_path)
+            return vlookup(item, csv_file_path, col_index_num, debug='fix')
+        if debug == 'skip':
+            if isinstance(item, Iterable) and not isinstance(item, str):
+                    return [column_dict[str(entry)] if str(entry) in column_dict else error_value for entry in item]
+            else:
+                return error_value
+        else:
+            raise
 
-def debug_lookup(entry,csv_file_path):
-    print 'LOOKUP ENTRY MISSING'
+def debug_lookup(entry, csv_file_path):
+    print('LOOKUP ENTRY MISSING')
     new_entry = [entry]
-    with open(csv_file_path,'rb') as csvfile:
+    with open(csv_file_path, 'r') as csvfile:
         reader = csv.reader(csvfile)
         for column_heading in next(reader)[1:]:
-            print 'Enter {0} for {1}:'.format(column_heading,entry)
-            column_entry = raw_input()
+            print('Enter {0} for {1}:'.format(column_heading, entry))
+            column_entry = input()
             new_entry.append(column_entry)
-    with open(csv_file_path,'ab') as csvfile:
+    with open(csv_file_path, 'a') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(new_entry)
--- vlookup_example.py
+++ vlookup_example.py
@@ -1,24 +1,25 @@
-import python_vlookup
+from __future__ import print_function
+from python_vlookup import python_vlookup
 
 value_to_lookup = 'bed-and-bath/accessories'
-value_list = ['bed-and-bath/accessories','bed-and-bath/storage-and-hampers/storage','new_entry_x','new_entry_y',1]
+value_list = ['bed-and-bath/accessories', 'bed-and-bath/storage-and-hampers/storage', 'new_entry_x', 'new_entry_y']
 
 def generate_values(value_list):
     for value in value_list:
         yield value
 
-### Basic VLOOKUP, just save your lookup tables as a csv and do it
-print python_vlookup.vlookup(value_to_lookup,'example.csv',2)
+# Basic VLOOKUP, just save your lookup tables as a csv and do it
+print(python_vlookup.vlookup(value_to_lookup, 'example.csv', 2))
 
-### Runs Through the debug
-print python_vlookup.vlookup(value_list,'example.csv',3,debug='skip')
+# Shows how to set a custom error_value
+print(python_vlookup.vlookup(value_list, 'example.csv', 3, debug='skip', error_value='#ERROR'))
 
-print python_vlookup.vlookup(value_list,'example.csv',3,debug='fix')
+# Runs Through the debug utility where you can fix your lookup table
+print(python_vlookup.vlookup(value_list, 'example.csv', 3, debug='fix'))
 
-
-### Faster_vlookup, can take in almost any iterable and returns an iterable
+# Faster_vlookup, can take in almost any iterable and returns an iterable
 csv_rows = python_vlookup.get_csv_data('example.csv')
-column_dict = python_vlookup.create_column_dict(csv_rows,1)
+column_dict = python_vlookup.create_column_dict(csv_rows, 1)
 
-for matched_value in python_vlookup.faster_vlookup(generate_values(value_list),column_dict):
-    print matched_value
+for matched_value in python_vlookup.faster_vlookup(generate_values(value_list), column_dict):
+    print(matched_value)
